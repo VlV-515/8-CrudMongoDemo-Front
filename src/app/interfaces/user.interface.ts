16bb211320d021020edb78b4dcd24eff6bd7bc69
VlV-515 <email@email.com>
2022-02-23 09:54:08 -0600

    Crea User Interface

---
+++ src/app/interfaces/user.interface.ts
@@ -0,0 +1,10 @@
+export interface UserInterface {
+  _id: string;
+  username: string;
+  pass: string;
+  name: string;
+  lastname: string;
+  email: string;
+  role: string;
+  dateIn: string;
+}
