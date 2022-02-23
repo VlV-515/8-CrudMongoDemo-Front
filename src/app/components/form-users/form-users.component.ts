c9d5c855388b15046342b23c9cc28e9dd1e2cee7
VlV-515 <email@email.com>
2022-02-23 11:02:39 -0600

    Estblece la logica para el control de la vista de form <viewForm>

--- src/app/components/form-users/form-users.component.ts
+++ src/app/components/form-users/form-users.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-form-users',
   templateUrl: './form-users.component.html',
-  styleUrls: ['./form-users.component.css']
+  styleUrls: ['./form-users.component.css'],
 })
 export class FormUsersComponent implements OnInit {
+  @Output() viewForm = new EventEmitter<boolean>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onCancel(): void {
+    this.viewForm.emit(false);
   }
-
 }
--- src/app/components/table-users/table-users.component.ts
+++ src/app/components/table-users/table-users.component.ts
@@ -1,15 +1,16 @@
+import { UserInterface } from './../../interfaces/user.interface';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-table-users',
   templateUrl: './table-users.component.html',
-  styleUrls: ['./table-users.component.css']
+  styleUrls: ['./table-users.component.css'],
 })
 export class TableUsersComponent implements OnInit {
+  viewForm: boolean = false;
+  dataForm?: UserInterface;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
